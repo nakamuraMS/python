1f1261239549d64157d6bc07a93148cf906e7c49
nakamura <nakamura@nanomedia.jp>
2020-06-11 17:13:15 +0900

    呼び出すモジュールの修正　バリデーション処理を追加

--- scraping/index.py
+++ scraping/index.py
@@ -1,11 +1,27 @@
-import argparse
+import sys
 import requests
+import re
+import logging
 from bs4 import BeautifulSoup
 
-args = sys.argv
-r = requests.get(args[1])
-soup = BeautifulSoup(r.content, 'html.parser')
+# バリデーション処理
+def validate(args):
+    if len(args) != 3:
+        logging.warning('引数の数が足りていません')
+        return False
+    elif not re.match("https?://[\w/:%#\$&\?\(\)~\.=\+\-]+", args[1]):
+        logging.warning('URLの形式が不正です')
+        return False
+    else:
+        return True
 
-element_count = len(soup.find_all(args[2]))
-print('要素数 : ' + str(element_count))
-print(soup.find_all(args[2]))
+args = sys.argv
+result = validate(args)
+if result :
+    r = requests.get(args[1])
+    soup = BeautifulSoup(r.content, 'html.parser')
+    element_count = len(soup.find_all(args[2]))
+    print('要素数 : ' + str(element_count))
+    print(soup.find_all(args[2]))
+else :
+    logging.error('処理が正常に実行されませんでした')
