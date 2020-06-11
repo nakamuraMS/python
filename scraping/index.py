6c4aa6bb1895302e06acf2efb60ae50d023bc741
nakamura <nakamura@nanomedia.jp>
2020-06-11 16:37:09 +0900

    コマンドライン引数を使用するように修正

--- scraping/index.py
+++ scraping/index.py
@@ -1,9 +1,11 @@
+import argparse
 import requests
 from bs4 import BeautifulSoup
 
-r = requests.get('https://www.sejuku.net/blog/51241')
+args = sys.argv
+r = requests.get(args[1])
 soup = BeautifulSoup(r.content, 'html.parser')
 
-element_count = len(soup.find_all('ul'))
+element_count = len(soup.find_all(args[2]))
 print('要素数 : ' + str(element_count))
-print(soup.find_all('ul'))
+print(soup.find_all(args[2]))
