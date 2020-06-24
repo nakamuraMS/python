290310c3bff21bcf63200a61408b4eca26ca88db
nakamura <nakamura@nanomedia.jp>
2020-06-24 18:11:28 +0900

    jupyter notebookで作成していく方向に転換

--- csv/index.py
+++
@@ -1,5 +0,0 @@
-import pandas as pd
-# 以下のデータを使用
-# https://www.e-stat.go.jp/stat-search/files?page=1&layout=datalist&toukei=00200521&tstat=000001011777&cycle=0&tclass1=000001094741&stat_infid=000031523105
-df = pd.read_csv("c03.csv", sep="," ,encoding="SHIFT-JIS")
-print(df.dropna())
